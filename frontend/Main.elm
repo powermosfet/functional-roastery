2b9644a751a87226dfbe5c1d1568e02ef9da3476
asmundberge <herrasbe@gmail.com>
2017-06-20 10:35:16 +0200

    add type annotation to main

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -2,10 +2,13 @@ module Main exposing (main)
 
 import Html
 import Init
+import Model
+import Message
 import Update
 import View
 
 
+main : Program Never Model.Model Message.Msg
 main =
     Html.program
         { init = Init.init
