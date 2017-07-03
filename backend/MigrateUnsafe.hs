59f3d107761cdc889f08d5326a310bd82673cd40
asmundberge <herrasbe@gmail.com>
2017-07-03 07:41:33 +0200

    add MigrateUnsafe

---
+++ backend/MigrateUnsafe.hs
@@ -0,0 +1,23 @@
+module Main where 
+
+import Database.Persist.Sql
+import System.Environment
+import System.IO
+
+import Model
+import Config
+
+main :: IO ()
+main = do
+    putStrLn "PERFORMING UNSAFE MIGRATION!!!!"
+    putStrLn "*******************************"
+    putStrLn ""
+    hSetBuffering stdout LineBuffering
+    env <- getEnvironment 
+    putStr "DATABASE_URL = "
+    print (lookup "DATABASE_URL" env)
+    let config = fromEnvironment env 
+    putStrLn "Using config:"
+    print config
+    pool <- makeDbPool config
+    runSqlPool (runMigration migrateAll) pool
